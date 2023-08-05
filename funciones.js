90f9e9364f8eff639f209ad88de20271cd9fc149
renucrerar <renatacrerar87@gmail.com>
2023-08-05 11:11:34 -0300

    agregamos storage

--- funciones.js
+++ funciones.js
@@ -1,43 +1,45 @@
-// traer un elemento con ID
-const btnEnviar = document.getElementById('btnEnviar');
+btnEnviar.addEventListener('click', function (event) {
+    event.preventDefault(); 
 
-
-btnEnviar.addEventListener('click', function (event) { // genera una accion en la pag, como click en un boton
-    event.preventDefault(); // Evitar el comportamiento por defecto del formulario
- 
-    
+    // Obtener los datos del cliente
     const nombreApellido = document.getElementById('nombreApellido').value;
     const edad = document.getElementById('edad').value;
     const dni = document.getElementById('dni').value;
 
-    // Luego puedes hacer lo que necesites con los valores ingresados
-    console.log("Nombre y Apellido: " + nombreApellido);
-    console.log("Edad: " + edad);
-    console.log("DNI: " + dni);
-
-
+    // Guardar los datos en el almacenamiento local
+    localStorage.setItem('nombreApellido', nombreApellido);
+    localStorage.setItem('edad', edad);
+    localStorage.setItem('dni', dni);
+ 
+    // Resetea el formulario para borrar los datos ingresados
+    document.getElementById('formularioIngreso').reset();
 });
 
-// Función para calcular el plan anual
+
+
+// Función para calcular el plan anual, con map recorro el arreglo planMensual para modificar uno por uno
 function calcularPlanAnual(planMensual) {
     return planMensual.map(item => ({
         dia: item.dia * 12,
-        precio: `$${(parseFloat(item.precio.slice(1)) * 12 * 0.9).toFixed(2)}`
+        precio: `$${(parseFloat(item.precio.slice(1)) * 12 * 0.9).toFixed(2)}`  /*esa linea de codigo la saque de internet, cumple
+        la funcion de cada "plan" multiplicarlo por 12 y restarle el %10, todavia hay cosas que estoy viendo como funcionan. */
+
     }));
 }
 
-// Obtener elementos del DOM
+// Llamamos con el ID "botones" u "opciones" del html
 const btnMensual = document.getElementById('btnMensual');
 const btnAnual = document.getElementById('btnAnual');
 const respuestaDiv = document.getElementById('respuesta');
 
 // Agregar eventos a los botones
-btnMensual.addEventListener('click', function () {
+btnMensual.addEventListener('click', function () { //agrego la funcion de hacer click
     const mensual = [
         { dia: 4, precio: "$6500" },
         { dia: 8, precio: "$9000" },
-        { dia: 12, precio: "$10500" }
+        { dia: 12, precio: "$10500" } //Pongo lo que quiero que muestre mi boton
     ];
+
 
     // Crear elementos para mostrar la lista de planes mensuales
     const ul = document.createElement('ul');
@@ -46,10 +48,10 @@ btnMensual.addEventListener('click', function () {
         li.textContent = `Día: ${item.dia}, Precio: ${item.precio}`;
         ul.appendChild(li);
     });
+ // Limpiar el contenido previo y mostrar la lista anterior
+ respuestaDiv.innerHTML = '';
+ respuestaDiv.appendChild(ul);
 
-    // Limpiar el contenido previo y agregar la lista al respuestaDiv
-    respuestaDiv.innerHTML = '';
-    respuestaDiv.appendChild(ul);
 });
 
 btnAnual.addEventListener('click', function () {
@@ -61,7 +63,7 @@ btnAnual.addEventListener('click', function () {
 
     const anual = calcularPlanAnual(mensual);
 
-    // Crear elementos para mostrar la lista de planes anuales
+   
     const ul = document.createElement('ul');
     anual.forEach(item => {
         const li = document.createElement('li');
@@ -69,26 +71,21 @@ btnAnual.addEventListener('click', function () {
         ul.appendChild(li);
     });
 
-    // Limpiar el contenido previo y agregar la lista al respuestaDiv
+   
     respuestaDiv.innerHTML = '';
     respuestaDiv.appendChild(ul);
 });
-
-
-
 
 const formulario = document.getElementById("formularioConocerMas");
 
 formulario.addEventListener("submit", (event) => {
     event.preventDefault();
-    const actividadFisica = document.getElementById("respuestaActividad").value.toLowerCase();
-    const objetivoGimnasio = document.getElementById("respuestaObjetivo").value;
+    const actividadFisica = document.getElementById("respuestaActividad").value.toLowerCase(); //llama a html para poder guardar la respuesta
+    const objetivoGimnasio = document.getElementById("respuestaObjetivo").value.toLowerCase();
 
     console.log("Respuestas del usuario:");
     console.log(`Actividad física: ${actividadFisica}`);
-    console.log(`Objetivo en el gimnasio: ${objetivoGimnasio}`);
+    console.log(`Objetivo en el gimnasio: ${objetivoGimnasio}`); //las imprime en consola
 
-    // Aquí puedes realizar cualquier acción adicional con las respuestas del usuario.
-
-    formulario.reset();
+    formulario.reset(); //se elimina de pantanlla los datos ingresados cuando apretas enviar
 });
